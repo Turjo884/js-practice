3ad7c5497b7bcdfbdc32bb7589db448c31621791
Turjo884 <hturjokhan@gmail.com>
2022-07-20 10:20:35 +0600

    learning this keyword

--- main.js
+++ main.js
@@ -359,3 +359,35 @@
 // catch(err){
 //     console.log("to Precision error")
 // }
+
+
+// learning this keyword and this is implicit function binding
+
+
+const person = {
+    firstName : "John",
+    lastName  : "Wick",
+    id        : 9389,
+    fullName  : function(){
+        return this.firstName + " " + this.lastName;
+    },
+    getFullName: function(){
+        return this.fullName();
+    }
+}
+
+console.log(person.getFullName());
+
+
+// explicit function binding
+const person1 = {
+    fullName : function(){
+        return this.firstName + " " + this.lastName; 
+    }
+}
+const person2 = {
+    firstName : "John",
+    lastName  : "Wick",
+}
+
+console.log(person1.fullName.call(person2));
